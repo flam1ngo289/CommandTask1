2a3c260059adcd15143e5863ca57888ff8fb31e1
flam1ngo <kris.s.03@mail.ru>
2024-09-15 11:10:01 +0300

    Initial commit

---
+++ CommandTask1/CommandTask1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class ConsoleLogCommand : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+class FileLogCommand : public LogCommand {
+public:
+    FileLogCommand(const std::string& filePath) : filePath(filePath) {}
+
+    void print(const std::string& message) override {
+        std::ofstream file(filePath, std::ios::app); 
+        if (file.is_open()) {
+            file << message << std::endl;
+            file.close();
+        }
+        else {
+            std::cerr << "Не удалось открыть файл: " << filePath << std::endl;
+        }
+    }
+
+private:
+    std::string filePath;
+};
+
+void print(LogCommand& logCommand, const std::string& message) {
+    logCommand.print(message);
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    ConsoleLogCommand consoleLog;
+    FileLogCommand fileLog("log.txt");
+
+    print(consoleLog, "Сообщение для консоли");
+
+    print(fileLog, "Сообщение для файла");
+
+    return 0;
+}
